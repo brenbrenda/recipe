0385a747f087966f8071b0d759d0a02fad6b4969
CHIYA <chia930429@gmail.com>
2021-03-10 22:28:25 +0800

    version2

--- recipe/ViewController.swift
+++ recipe/ViewController.swift
@@ -11,9 +11,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var visualEffectView: UIVisualEffectView!
     @IBOutlet var addListView: UIView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var TurnOnOff: UISwitch!
+    @IBOutlet weak var Slider: UISlider!
     var effect: UIVisualEffect!
     override func viewDidLoad() {
-       
         effect = visualEffectView.effect
         visualEffectView.effect = nil
         //addListView.layer.cornerRadius = 10
@@ -22,6 +25,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+
+
+    @IBAction func OPEN(_ sender: UISwitch) {
+        if sender.isOn == true {
+            view.backgroundColor = #colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)
+        } else
+        {
+            view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+    }
+    
+    
+ 
+
+    @IBOutlet weak var labell: UILabel!
+    var fontSize: Float = 0.0
+    @IBAction func SliderValue(_ sender: UISlider) {
+        let senderValue = CGFloat(sender.value)
+        labell.font = UIFont(name: labell.font.fontName, size: senderValue)
+    }
+    @IBAction func changePage(_ sender: UIPageControl) {
+        let point = CGPoint(x: scrollView.bounds.width * CGFloat(sender.currentPage), y :0)
+        scrollView.setContentOffset(point, animated: true)
+    }
     func animatedIn() {
         self.view.addSubview(addListView)
         addListView.center = self.view.center
@@ -48,7 +75,12 @@ class ViewController: UIViewController {
     @IBAction func dissmissPopup(_ sender: Any) {
         animatedOut()
     }
-
-
+}
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidEndDecelerating(_ scrollView:UIScrollView) {
+        let page = scrollView.contentOffset.x / scrollView.bounds.width
+        pageControl.currentPage = Int(page)
+        
+    }
 }
 
---
+++ recipe/recipeViewController.swift
@@ -0,0 +1,71 @@
+//
+//  recipeViewController.swift
+//  recipe
+//
+//  Created by chia on 2021/3/10.
+//
+
+import UIKit
+import WebKit
+
+class recipeViewController: UIViewController {
+
+    @IBOutlet weak var myRecipe: WKWebView!
+    @IBOutlet weak var Reload: UIActivityIndicatorView!
+    let Str1 = "https://www.goodhousekeeping.com/food-recipes/easy/g871/quick-breakfasts/"
+    let Str2 = "https://www.allrecipes.com/recipe/279909/broiled-paprika-and-lemon-pepper-chicken-breasts/"
+    let Str3 = "https://www.allrecipes.com/gallery/fancy-casseroles/"
+   
+    @IBAction func mySegmentedAction(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 {
+            Reload.startAnimating()
+            if let url = URL(string: Str1) {
+                let request = URLRequest(url: url)
+                myRecipe.load(request)
+                Reload.stopAnimating()
+            }
+        } else if sender.selectedSegmentIndex == 1 {
+            Reload.startAnimating()
+            if let url = URL(string: Str2) {
+                let request = URLRequest(url: url)
+                myRecipe.load(request)
+                Reload.stopAnimating()
+            }
+        } else if sender.selectedSegmentIndex == 2 {
+            Reload.startAnimating()
+            if let url = URL(string: Str3) {
+                let request = URLRequest(url: url)
+                myRecipe.load(request)
+                Reload.stopAnimating()
+            }
+        }
+    }
+    
+    /*func Recipe(_ Recipe: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        Reload.startAnimating()
+    }
+    
+    func Recipe(_ Recipe: WKWebView, didFinish navigation: WKNavigation!) {
+        Reload.stopAnimating()
+    }*/
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
